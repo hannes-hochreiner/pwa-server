3789026279e52941e36658aa327c3cad3fe3e1bb
Hannes Hochreiner <hannes@hochreiner.net>
2021-12-19 09:49:35 +0100

    add serving of static data; fix #1

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A server for progressive web apps.
 
+## Configuration
+
+PWA-Server expects the following environment variables:
+
+|Name | Description|
+|----|----|
+|ROOT_FS_PATH| location of the folder containing the static content served at "/"|
+
+
 ## License
 
 This work is licensed under the MIT license.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-#[macro_use] extern crate rocket;
+use std::env;
+
+use rocket::fs::FileServer;
+
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -7,5 +12,7 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/config", routes![index])
+        .mount("/", FileServer::from(env::var("ROOT_FS_PATH").unwrap()))
 }
